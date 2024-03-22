26b7f7e7b8c7b5a5a4686462e3366e002647a521
callyb99 <callyb201@gmail.com>
2024-03-22 21:41:08 +0100

    ccyhcghhjn

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [query, setQuery] = useState('')
+  const [todo, setTodo] = useState([])
+
+  function addTodo() {
+    if(!query) return
+    setTodo([...todo, {
+      item: query,
+      id: Date.now().toLocaleString(),
+      isDone: false,
+    }])
+   setQuery('')
+  }
+
+  function deleteTodo(id) {
+     const newList = todo.filter(todo=> todo.id !== id)
+
+     setTodo(newList)
+  }
+  return <div className="container">
+  <h1>To-do List</h1>
+  <input type="text" value={query} onChange={e=>setQuery(e.target.value)} id="taskInput" placeholder="Enter task..." />
+  <button onClick={addTodo} >Add</button>
+  <ul id="taskList">
+
+{todo.map(todo =>
+  <li key={todo.id}>
+    <span>{todo.item}</span>
+
+    <span>
+      <button onClick={()=>deleteTodo(todo.id)} >Delete</button>
+    </span>
+</li>)}
+  {/*  */}
+  </ul>
+</div>
 }
 
-export default App;
+export default App
